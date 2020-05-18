38c0d3ab1ba6d26d65f5900139cd5d8bf2a4665f
danfrgo <43517221+danfrgo@users.noreply.github.com>
2020-05-18 22:59:08 +0100

    MySql adaptation and first migration

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -37,7 +37,9 @@ namespace SalesWebMvc
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebMvcContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), 
+                        builder => builder.MigrationsAssembly("SalesWebMvc")));
+            // builder.MigrationsAssembly("SalesWebMvc"))); - SalesWebMvc é o nome do projeto e deve estar entre ()
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
